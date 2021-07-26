0a64361654cacb10f90c9d38f88a73d135092468
dlstn4509 <dlstn7609@hanmail.net>
2021-07-26 20:29:35 +0900

    저장

--- 백업/public/js/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function(e) {
-	var sct = this.scrollY;
-	if(sct > 100) {
-		document.querySelector('.header-wrapper').classList.add('active');
-	}
-	else {
-		document.querySelector('.header-wrapper').classList.remove('active');
-	}
-})
