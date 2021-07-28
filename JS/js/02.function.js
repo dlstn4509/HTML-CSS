421768f8368c54369d6d2c004c315220d9e2f74e
dlstn4509 <dlstn7609@hanmail.net>
2021-07-28 15:02:09 +0900

    object 작업중

--- JS/js/02.function.js
+++ JS/js/02.function.js
@@ -1,6 +1,17 @@
 var a = 10;
 var b = 20;
 
-function sum () {
-    console.log(a,b,c)
+function sum(c, d) {
+    console.log(a, b, c, d);
+}
+// onclick="sum();" -> 10, 20, undefined, undefined
+// onclick="sum(30, 40);" -> 10, 20, 30, 40
+
+// 리턴값이 있는 함수
+// onclick="console.log(sum2(40, 50))";
+// onclick="console.log(90)";
+// 함수에서 return을 만나면 함수가 값을 리턴하고 종결한다.
+function sum2(a, b) {
+    return a + b;
+    return a * b; // 이건 적용 안댐
 }
