a3f2cb3441d148ed08bcd669271df509837132d0
dlstn4509 <dlstn7609@hanmail.net>
2021-07-29 17:20:21 +0900

    random, Array

--- JS/js/04.if.js
+++ JS/js/04.if.js
@@ -1,13 +1,45 @@
 /* 
 - 비교연산자
-==, ===, <=, >=, <, > (===은 타입까지 같아야 함). !=. !==
+==, <=, >=, < , >, === (타입까지 같아야 함), != , !==
 
 - 내장객체
 window, console, String, Number, Array, Object, Date, Math
-window.document
+
+- 변수의 타입
+var b = 10;
+var c = '10'
+var d = function() {}
+var e = true
+var f = {}
+
+typeof b    =>    "number"
+typeof c    =>    "string"
+typeof d    =>    "function"
+typeof e    =>    "boolean"
+typeof f    =>    "object"
 */
 
+/*
+var a = 8;
+if(typeof a !== 'number') {
+	console.log('변수 a는 숫자가 아닙니다.');
+}
+else if(a < 10) {
+	console.log('변수 a는 10 미만의 수 입니다.');
+}
+else if(a > 10) {
+	console.log('변수 a는 10 초과의 수 입니다.');
+}
+else {
+	console.log('변수 a는 10 입니다.')
+}
+*/
+
+
+// window.alert('hi');
 console.log(window);
+console.log(window.document);
+console.log(document);
 console.log(console);
 console.log(String);
 console.log(Number);
@@ -16,50 +48,30 @@ console.log(Object);
 console.log(Date);
 console.log(Math);
 
-/* 
-var a=10;
-if(typeof a !== 'number') {
-  console.log(`변수 a는 숫자가 아닙니다.`)
-}
-else if(a<10) {
-  console.log(`변수 a는 10 미만의 수 입니다.`);
-}
-else if(a<10) {
-  console.log('변수 a는 10 이상의 수 입니다.');
-}
-
-else {
-  console.log (`변수 a는 10 입니다.`);
-}
-*/
-
-
-
 /* function changeColor(value) {
-  // alert(value === '');
-  console.log(document.getElementById('box'))
-  if(value === '') {
-    console.log('value가 빈문자열 입니다.');
-    document.getElementById('box').style.backgroundColor = 'transparent';
-  }
-  else {
-    console.log('value는' + value + '입니다.');
-    document.getElementById('box').style.backgroundColor = value;
-  }
+	// alert(value === '');
+	console.log(document.getElementById('box'));
+	if(value === '') {
+		console.log('value는 빈문자열 입니다.');
+		document.getElementById('box').style.backgroundColor = 'transparent';
+	}
+	else {
+		console.log('value는 ' + value + '입니다.');
+		document.getElementById('box').style.backgroundColor = value;
+	}
 } */
 
 function changeColor(value) {
-  console.log(document.getElementById('box'))
-  if(value === '') {
-    document.getElementById('box').style.backgroundColor = 'transparent';
-  }
-  else {
-    document.getElementById('box').style.backgroundColor = value;
-  }
+	if(value === '') {
+		document.getElementById('box').style.backgroundColor = 'transparent';
+	}
+	else {
+		document.getElementById('box').style.backgroundColor = value;
+	}
 }
 
 console.clear();
 
 function showImg(src) {
-  document.getElementById('mainImg').src = src;
+	document.getElementById('mainImg').src = src;
 }
--- JS/js/06.box.js
+++ JS/js/06.box.js
@@ -5,21 +5,20 @@
 */
 
 
-var i =1;
-var sum = 0;
-for(; i<=10; i++) {
-  sum += i  // sum = sum + i
-}
+// var i =1;
+// var sum = 0;
+// for(; i<=10; i++) {
+//   sum += i  // sum = sum + i
+// }
 
 
 
-
-function createBox () {
-  document.getElementById('stage').innerHTML += '<div class="box"></div>';
-}
-function removeBox () {
-  document.getElementById('stage').innerHTML = '';
-}
+// function createBox () {
+//   document.getElementById('stage').innerHTML += '<div class="box"></div>';
+// }
+// function removeBox () {
+//   document.getElementById('stage').innerHTML = '';
+// }
 
 // var stage = document.getElementById('stage');
 // function createBox () {
@@ -28,3 +27,30 @@ function removeBox () {
 // function removeBox () {
 //   stage.innerHTML = '';
 // }
+
+
+
+
+
+/* 
+기능정의서
+1. changebox('create')가 실행되면 #stage에 .box를 생성한다.
+2. chnageBox('remove')가 실행되면 #stage에 .box를 삭제한다.
+3. #count의 값만큼 .box를 생성한다.
+*/
+
+var stage = document.getElementById('stage');
+
+
+var count = document.getElementById('count');
+// var color = ['red', 'green', 'blue', 'orange', 'purple'];
+function changeBox (opt) {
+  if(opt === 'create') {
+    for(var i=1; i<=count.value; i++)
+    stage.innerHTML += '<div class="box"></div>';
+  }
+  else {
+    stage.innerHTML = '';
+    count.value = 1;
+  }
+}
