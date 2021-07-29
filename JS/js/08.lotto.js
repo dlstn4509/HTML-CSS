0e5d04efa242e88fa09467117b6aef5f18461aa9
dlstn4509 <dlstn7609@hanmail.net>
2021-07-29 17:34:05 +0900

    Array

--- JS/js/08.lotto.js
+++ JS/js/08.lotto.js
@@ -31,27 +31,48 @@ Array
 - arr.length : 배열요소의 갯수
 - arr.push('값') : 배열의 맨 뒤에 값을 넣는다.
 - arr.unshift('값') : 배열의 맨 앞에 값을 넣는다.
-- arr.pop(): 배열의 맨 뒤의 값을 뽑아낸다 (배열이 변한다.)
-- arr.shift(): 배열의 맨 앞의 값을 뽑아낸다 (배열이 변한다.)
+- arr.pop() : 배열의 맨 뒤의 값을 뽑아낸다 (배열이 변한다.)
+- arr.shift() : 배열의 맨 앞의 값을 뽑아낸다 (배열이 변한다.)
+- arr.splice(idx, 빼고싶은 갯수, 넣을 값) : 배열의 중간에 값을 넣거나 빼거나 교체한다. 
  */
 
 var arr = ['A', 'B'];
-console.log(arr);            //  ['A', 'B']
+
+console.log(arr);                      //  ['A', 'B']
 
 arr.push('C');               
-console.log(arr);            //  ['A', 'B', 'C']
+console.log(arr);                      //  ['A', 'B', 'C']
 
 arr.unshift('Z')             
-console.log(arr);            //  ['Z', 'A', 'B', 'C']
+console.log(arr);                      //  ['Z', 'A', 'B', 'C']
+
+console.log(arr[2]);                   // B
+console.log(arr);                      // ['Z', 'A', 'B', 'C']
+
+console.log(arr.pop())                 // C 
+console.log(arr);                      // ['Z', 'A', 'B']
+
+console.log(arr.shift())               // Z 
+console.log(arr);                      // ['A', 'B']
 
 
-console.log(arr[2]);         // B
-console.log(arr);            // ['Z', 'A', 'B', 'C']
 
-console.log(arr.pop())       // C 
-console.log(arr);            // ['Z', 'A', 'B']
+var fruits = ['apple', 'banana', 'cherry']
+// 값을 넣기만 함
+console.log(fruits.splice(1, 0, 'melon')) // [] (꺼내는 값들을 새로운 배열에 리턴한다)
+console.log(fruits);                      // ['apple', 'melon', 'banana', 'cherry']
 
-console.log(arr.shift())     // Z 
-console.log(arr);            // ['A', 'B']
+var fruits = ['apple', 'banana', 'cherry']
+// 값을 빼기만 함
+console.log(fruits.splice(1, 1))          // ['banana'] 
+console.log(fruits);                      // ['apple', 'cherry']
 
+var fruits = ['apple', 'banana', 'cherry']
+// 값을 교체 함
+console.log(fruits.splice(1, 1, 'melon', 'orange'))  // ['banana'] 
+console.log(fruits);                                 // ["apple", "melon", "orange", "cherry"]
 
+var fruits = ['apple', 'banana', 'cherry']
+// 값을 교체 함
+console.log(fruits.splice(1, 2, 'melon', 'orange'))  // ["banana", "cherry"]
+console.log(fruits);                                 // ["apple", "melon", "orange"]
