942bb89cabf1eb17660e3758bdde53ec3aec9725
dlstn4509 <dlstn7609@hanmail.net>
2021-08-02 13:03:09 +0900

    5번 갤러리 다시

--- jquery/js/05.gallery.js
+++ jquery/js/05.gallery.js
@@ -62,44 +62,37 @@ var products = [
 	},
 ]
 
+$(document).ready(function() {
 
-var json = JSON.stringify(products); // js Object -> json
-var jsobj = JSON.parse(json); //  json -> js Object
-
-
-(function init() {
-	var cnt = products.length; // 12
-	var html = '';
-	var wrap = document.getElementsByClassName('prd-wrap')[0];
-	for(var i=0; i<cnt; i++) {
+	for(var i=0, html; i<products.length; i++) {
 		html = '<li class="prd">';
 		html += '<div class="imgs">';
-		html += '<img src="'+products[i].src+'" class="w100" onclick="openModal('+i+');">';
+		html += '<img src="'+products[i].src+'" class="w100">';
 		html += '</div>';
 		html += '<div class="conts">';
 		html += '<h2 class="name">'+products[i].name+'</h2>';
-		html += '<p class="content">'+products[i].content+'</p>';
-		html += '<div class="price">$'+products[i].price+'</div>';
+		html += '<p class="conaent">'+products[i].content+'</p>';
+		html += '<div class="price">'+products[i].price+'</div>';
 		html += '</div>';
 		html += '</li>';
-		wrap.innerHTML += html;
+		$('.prd-wrap').append(html); // prepend 는 역순, $('.prd-wrap')[0].innerHTML += html;
 	}
-})();  //  IIFE(즉시실행함수)
+		// 여기까지 와야 .prd 생성완료
+		$('.prd img').click(function(){
+			var src = $(this).attr('src'); // this.src; (Getter)
+			$('.modal-wrap img').attr('src', src); // this.src; (Setter)
+			$('.madal-wrapper').stop().fadein(800); // this.style.display = 'flex'; (Setter)
+		});
+		$('.modal-wrapper .bt-close').click(function(){
+			$('.modal-wrapper').stop().fadeOut(800);
+		});
+});
 
-function openModal(id) {
-	var modal = document.getElementsByClassName('modal-wrapper')[0];
-	var img = modal.getElementsByTagName('img')[0];
-	img.src = products[id].src;
-	modal.style.display = 'flex';
-}
+		// document.getElementsByClassName('prd-wrap')[0].innerHTML += html;
+		// $('.prd-wrap')[0].innerHTML += html;
 
+		// $('.prd-wrap').html(); // $('.prd-wrap')[0].innerHTML; (Getter)
+		// $('.prd-wrap').html(html); // $('.prd-wrap')[0].innerHTML = html; (Setter)
 
-
-function closeModal() {
-	var modal = document.getElementsByClassName('modal-wrapper')[0];
-	modal.style.display = 'none';
-}
-/* 1. bt-close를 클릭하면 modal창을 닫는다. */
-
-
-
+		// $('.prd-wrap').html( $('.prd-wrap').html() + html ); // $('.prd-wrap')[0].innerHTML += html;
+		// $('.prd-wrap').append(html); // $('.prd-wrap')[0].innerHTML += html;
