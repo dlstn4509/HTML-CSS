3fea870ceab74e955461aa0b6c08d0e45a2c1444
dlstn4509 <dlstn7609@hanmail.net>
2021-08-03 12:22:06 +0900

    date 시계 만들기

--- 0.BASE/05_util.js
+++ 0.BASE/05_util.js
@@ -3,4 +3,11 @@ function random(startNumber, count) {
 	return Math.floor( Math.random() * count ) + startNumber;
 }
 
-
+// 0 ~ 9를 받으면 '0'을 더해주는 함수
+function zp(n) {
+	// if(n<10) return '0'+n;
+	// else return n;
+	
+	return (n < 10) ? '0'+n : n;
+	// 삼항 연산자 let a = 조건 ? 실행(참) : 실행(거짓)
+}
--- jquery/js/10.colck.js
+++ jquery/js/10.colck.js
@@ -1,1 +1,25 @@
-let arr = [];
+let d = new Date(); // 객체의 Instance
+console.log(d);
+console.log(d.__proto__);
+console.log(d.getFullYear());       // 2021
+console.log(d.getMonth());          // 0 ~ 11
+console.log(d.getDate());           // 3
+console.log(d.getDay());            // 0(Sunday) ~ 6
+console.log(d.getHours());          // 24시간제 (0 ~ 23)
+console.log(d.getMinutes());        // 0 ~ 59
+console.log(d.getSeconds());        // 0 ~ 59
+
+let nowDate = d.getFullYear() + '-' + zp((d.getMonth() + 1)) + '-' + zp(d.getDate()) + ' ' + zp(d.getHours()) + ':' + zp(d.getMinutes()) + ':' + zp(d.getSeconds());
+
+console.log(nowDate);  //  ISO 표준시 (국제 표준시간 표기)
+
+
+setInterval(oninterval, 1000);
+function oninterval() {
+  let yoil = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  let d = new Date();
+  let date = d.getFullYear() + '-' + zp((d.getMonth() + 1)) + '-' + zp(d.getDate()); 
+  let time = zp(d.getHours()) + ':' + zp(d.getMinutes()) + ':' + zp(d.getSeconds());
+  document.querySelector('.date-wp').innerHTML = date + '. ' + yoil[d.getDay()];
+  document.querySelector('.time-wp').innerHTML = time;
+}
