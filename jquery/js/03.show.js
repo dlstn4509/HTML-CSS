f2dff0cda3e538585f871648edafc473b30b1d11
dlstn4509 <dlstn7609@hanmail.net>
2021-08-03 10:24:13 +0900

    03 show 버튼 추가

--- jquery/js/03.show.js
+++ jquery/js/03.show.js
@@ -2,8 +2,11 @@
 // hide() 괄호 안에 시간을 주면 시간동안 width, height, opacity가 줄어들면서 width, height, opacity가 0이 되면서 사라짐 ("display: none;")  
 // "slow"(600), "normar"(400), "fast"(200), ms (시간 표기법)
 
-$('.bt1').on('click', function() {
-  $('.box').stop().hide(1000);
+$('.bt1').click(function() {
+  $('.box').stop().hide(1000, function() {
+    // callback 함수
+    alert('사라졌어요');
+  });
 })
 
 $('.bt2').on('click', function() {
@@ -26,11 +29,19 @@ $('.bt6').on('click', function() {
   $('.box').stop().slideDown(1000);
 })
 
-$('.bt7').on('click', function() {
-  $('.box').stop();
+$('.bt-ani').click(function() {
+  $('.box').stop().fadeOut(1000, function(){
+    $(this).stop().fadeIn(1000, function(){
+      $(this).stop().slideUp(1000, function(){
+        $(this).stop().slideDown(1000, function(){
+          alert('애니메이션 종료');
+        })
+      })
+    })
+  });
 })
 
-$('.navi').on('mouseenter', function () {
+$('.bt-ani').on('mouseenter', function () {
   // $(this).childern('.sub-wp').stop().slideDown(100); // 자식선택자
   $(this).find('.sub-wp').stop().slideDown(100);        // 자손선택자
 })
@@ -38,3 +49,24 @@ $('.navi').on('mouseenter', function () {
 $('.navi').on('mouseleave', function () {
   $(this).find('.sub-wp').stop().slideUp(100);
 })
+
+
+$('.bt7').click(function() {
+  $('.box3').stop().animate({"left":"1000px"}, 2000, 'swing')     
+  $('.box2').stop().animate({"left":"1000px"}, 2000, 'linear', function(){
+    alert('ani end');
+  });     
+  // (json형태의css, [ms시간, 문자열('swing', 'linear'), fn(){}]);
+  // [] 안에는 생략 가능
+});
+
+$('.bt-8').click(function(){
+  $('.box4').stop().animate({
+    "width":"500px",
+    "height":"500px"
+  })
+});
+
+$('.bt9').click(function(){
+  $('.box5').addClass('active');
+});
