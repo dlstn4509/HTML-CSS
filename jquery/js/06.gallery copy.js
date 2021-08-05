de3acf0b11256ff15839806ba88c93b7de59b3bc
dlstn4509 <dlstn7609@hanmail.net>
2021-08-05 17:57:36 +0900

    학원 복습 끝

--- jquery/js/06.gallery copy.js
+++ jquery/js/06.gallery copy.js
@@ -8,13 +8,17 @@ var foods = [
   { name : 'Croissant', src:'../img/f7.jpg'}
 ];
 
-for(var i=0, html; i<foods.length; i++){
+var list = document.querySelector('.list')
+
+for (var i=0, html; i<foods.length; i++) {
   html = '<li class="list">'
-  html += '<img src="'+foods[i].src+'" class="w100 thumb" alt="'+foods[i].name+'">'
-  html += '</li>'
-  $('.list-wrap').append(html);
+  html += '<img src="'+foods[i].src+'" alt="'+foods[i].name+'" class="w100 thumb"></li>'
+  $('.list-wrap').append(html)
 }
+
 $('.list-wrap .thumb').click(function(){
-  $('.stage-wrap .big').attr('src', $(this).attr('src'));
-  $('.stage-wrap .name').html($(this).attr('alt'));
-});
+  $('.stage-wrap img').attr('src', this.src)
+  $('.stage-wrap .name').html(this.alt)
+  
+})
+
