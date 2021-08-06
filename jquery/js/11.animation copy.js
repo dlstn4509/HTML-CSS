1bee3bdc317dad863332094d9f2bd7583e7b1c9b
dlstn4509 <dlstn7609@hanmail.net>
2021-08-06 13:20:05 +0900

    11 박스 다시오기 작업중

--- jquery/js/11.animation copy.js
+++ jquery/js/11.animation copy.js
@@ -1,8 +1,8 @@
 
 /* document.querySelector('.bt-hide').addEventListener("click", function(){
   $(document.querySelector('.box')).fadeOut(1000);
-})
- */
+}) */
+
 
 $('.bt-hide').click(function(){
   $('.box').fadeOut();
@@ -10,4 +10,9 @@ $('.bt-hide').click(function(){
 
 $('.bt-show').click(function(){
   $('.box').fadeIn();
+})
+
+$('.bt-move').click(function(){
+  $('.box2').animate({left:"1000px"}, 100);
+  $('.box2').animate({right:"1000px"}, 200);
 })
