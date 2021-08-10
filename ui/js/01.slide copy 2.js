aa65806b89d0b4281a7f4a54b7ff910475cde0d3
dlstn4509 <dlstn7609@hanmail.net>
2021-08-10 22:24:09 +0900

    slide 가로 완성

--- ui/js/01.slide copy 2.js
+++ ui/js/01.slide copy 2.js
@@ -4,15 +4,17 @@ var $slidewrap = $wrapper.find('.slide-wrap');
 var $pager = $wrapper.find('.pager');
 var $btPrev = $wrapper.find('.bt-prev')
 var $btNext = $wrapper.find('.bt-next')
-var interval;                  // setInterval을 넣어놓을 변수
-var idx = 0;                   // animation이 움질일 값의 인자 (0, -100%, -200% ...)
-var gap = 2000;                // setInterval의 간격
-var speed = 500;               // animation speed
+var interval;           // setInterval을 넣어놓을 변수
+var idx = 0;            // animation이 움질일 값의 인자 (0, -100%, -200% ...)
+var gap = 2000;         // setInterval의 간격
+var speed = 500;        // animation speed
 var last = $slidewrap.find('.slide').length -1;    // $('.slide)의 마지막 index 6
 
 /*************** user function ******************/
 function ani() {
   $slidewrap.stop().animate({"left" : -idx * 100 + '%'}, speed)
+  $pager.removeClass('active');
+  $pager.eq(idx === last ? 0 : idx).addClass('active');
 }
 
 /*************** event callback *****************/
@@ -25,7 +27,8 @@ function onLeave() {
 }
 
 function onPager() {
-
+  idx = $(this).index();
+  ani();
 }
 
 function onPrev() {
@@ -42,7 +45,6 @@ function onNext() {
     idx = 0;
     $slidewrap.css({"left" : 0});
   }
-  // idx = idx + 1;
   idx++;
   ani();
 }
