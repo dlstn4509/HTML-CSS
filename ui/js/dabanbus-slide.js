4166782e61660689e5f8ec66777dd399892898f5
dlstn4509 <dlstn7609@hanmail.net>
2021-08-11 17:47:25 +0900

    ..

--- ui/js/dabanbus-slide.js
+++ ui/js/dabanbus-slide.js
@@ -1,6 +1,5 @@
 function Slide(_parent, _opt) {
 	var opt = _opt || {};
-	var obj = this;
 	this.interval; 
 	this.last; 
 	this.idx = 0; 
@@ -12,18 +11,20 @@ function Slide(_parent, _opt) {
 	this.speed = Number(opt.speed) || 500;
 	this.autoPlay = opt.autoPlay === false ? false : true;
 	this.autoPlaySpeed = Number(opt.autoPlaySpeed) || 3000;
+	this.useNavigation = opt.navigation && opt.navigation.prev && opt.navigation.next ? true : false;
+	this.usePagination = opt.pagination && opt.pagination.el ? true : false;
+
 	if(this.effect.toLowerCase() !== 'horizontal' && this.effect.toLowerCase() !== 'vertical' && this.effect.toLowerCase() !== 'fade') {
 		this.effect = 'horizontal';
 	}
 	this.effect += 'Type';
 	this.wrap.addClass(this.effect);
 
-
 	/******* horiInit *******/
 	var horiInit = function () {
 		this.last = this.slide.length - 1;
 
-		this.wrap.mouseenter(onEnter).mouseleave(onLeave);
+		this.wrapper.mouseenter(onEnter).mouseleave(onLeave);
 		if(this.autoPlay) this.interval = setInterval(this.onNext.bind(this), this.autoPlaySpeed);
 	}.bind(this);
 
@@ -31,7 +32,7 @@ function Slide(_parent, _opt) {
 	var fadeInit = function () {
 		this.last = this.slide.length - 1;
 
-		this.wrap.mouseenter(onEnter).mouseleave(onLeave);
+		this.wrapper.mouseenter(onEnter).mouseleave(onLeave);
 		if(this.autoPlay) this.interval = setInterval(this.onNext.bind(this), this.autoPlaySpeed);
 	}.bind(this);
 
@@ -39,7 +40,7 @@ function Slide(_parent, _opt) {
 	var vertInit = function () {
 		this.last = this.slide.length - 1;
 
-		this.wrap.mouseenter(onEnter).mouseleave(onLeave);
+		this.wrapper.mouseenter(onEnter).mouseleave(onLeave);
 		if(this.autoPlay) this.interval = setInterval(this.onNext.bind(this), this.autoPlaySpeed);
 	}.bind(this);
 
@@ -71,26 +72,49 @@ function Slide(_parent, _opt) {
 		this.slide = this.wrap.find('.slide');
 		vertInit();
 	}
+	/******* navigation *******/
+	if(this.useNavigation) {
+		this.prev = this.wrapper.find(opt.navigation.prev);
+		this.next = this.wrapper.find(opt.navigation.next);
+		this.prev.click(this.onPrev.bind(this));
+		this.next.click(this.onNext.bind(this));
+	}
+	/******* pagination *******/
+	if(this.usePagination) {
+		this.pagers = this.wrapper.find(opt.pagination.el);
+		var cnt = (this.effect === 'fadeType') ? this.slide.length : this.slide.length - 1;
+		for(var i=0; i<cnt; i++) {
+			$('<div>●</div>').appendTo(this.pagers).click(this.onPager.bind(this));
+		}
+	}
 }
 
 Slide.prototype.ani = function () {
 	switch(this.effect) {
 		case 'horizontalType':
 			this.wrap.stop().animate({ 'left': -this.idx * 100 + '%' }, this.speed);
-
+			if(this.usePagination) {
+				this.pagers.children('div').removeClass('active');
+				this.pagers.children('div').eq(this.idx === this.last ? 0 : this.idx).addClass('active');
+			}
 			break;
 		case 'verticalType':
 			this.wrap.stop().animate({ 'top': -this.idx * 100 + '%' }, this.speed);
-
+			if(this.usePagination) {
+				this.pagers.children('div').removeClass('active');
+				this.pagers.children('div').eq(this.idx === this.last ? 0 : this.idx).addClass('active');
+			}
 			break;
 		case 'fadeType':
 			this.slide.eq(this.idx).css({ 'z-index': ++this.depth, 'opacity': 0 });
 			this.slide.eq(this.idx).stop().animate({'opacity': 1}, this.speed);
-
+			if(this.usePagination) {
+				this.pagers.children('div').removeClass('active');
+				this.pagers.children('div').eq(this.idx).addClass('active');
+			}
 			break;
 	}
 }
-
 
 Slide.prototype.onNext = function() {
 	if(this.effect === 'fadeType') {
@@ -98,7 +122,7 @@ Slide.prototype.onNext = function() {
 	}
 	else {
 		if(this.idx === this.last) {
-			this.wrap.css('left', 0);
+			this.wrap.css(this.effect === 'horizontalType' ? 'left' : 'top' , 0);
 			this.idx = 0;
 		}
 		this.idx++;
@@ -107,11 +131,22 @@ Slide.prototype.onNext = function() {
 }
 
 Slide.prototype.onPrev = function() {
-
+	if(this.effect === 'fadeType') {
+		this.idx = this.idx === 0 ? this.last : this.idx - 1;
+	}
+	else {
+		if(this.idx === 0) {
+			this.wrap.css(this.effect === 'horizontalType' ? 'left' : 'top', -this.last * 100 + '%');
+			this.idx = this.last;
+		}
+		this.idx--;
+	}
+	this.ani();
 }
 
-Slide.prototype.onPager = function() {
-
+Slide.prototype.onPager = function(e) {
+	this.idx = $(e.target).index();
+	this.ani();
 }
 
 
