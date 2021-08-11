0b4c7aeef093178678d1c6c885a844e7d82d926e
dlstn4509 <dlstn7609@hanmail.net>
2021-08-11 10:58:50 +0900

    swiper

---
+++ ui/js/05.swiper.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".my-swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    // dynamicBullets: true,
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".my-swiper2", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+    // dynamicBullets: true,
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".my-swiper3", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    // dynamicBullets: true,
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
