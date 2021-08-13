3703e79e8a4c1ad298e5a631de429037adce7430
dlstn4509 <dlstn7609@hanmail.net>
2021-08-13 10:31:34 +0900

    02 search

--- ajax/js/01.ajax.js
+++ ajax/js/01.ajax.js
@@ -1,5 +1,5 @@
-/******************* 순수 자바스크립트 통신 *******************/
-/* 
+/******************* 순수 자바스크립트 통신 *******************/ // 바닐라 스크립트
+
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {       // 통신상태가 변하면 실행
   if(xhr.status === 200 && xhr.readyState === 4) {
@@ -10,12 +10,12 @@ xhr.onreadystatechange = function() {       // 통신상태가 변하면 실행
 xhr.open('GET', 'https://dapi.kakao.com/v2/search/web?query=블랙핑크');
 xhr.setRequestHeader('Authorization', 'KakaoAK accdfd5267af756d07efcd007e13bcee');
 xhr.send();
-*/
 
-/*************************** jQuery ***************************/
-/* 
+
+/*************************** jQuery.ajax ***************************/
+
 $.ajax({
-  url: 'https://dapi.kakao.com/v2/search/web',
+  url: 'https://dapi.kakao.com/v2/search/image',
   type: 'GET',
   dataType: 'json',
   data: {query: '블랙핑크'},
@@ -35,12 +35,12 @@ function onSuccess(v) {
 function onError(xhr, status, error) {
   console.log(xhr, status, error);
 };
-*/
+
 
 /*************************** axios ***************************/
 
 axios
-.get('https://dapi.kakao.com/v2/search/web', {
+.get('https://dapi.kakao.com/v2/search/vclip', {
   params: {query: '블랙핑크'},
   headers: {Authorization: 'KakaoAK accdfd5267af756d07efcd007e13bcee'}
 })
