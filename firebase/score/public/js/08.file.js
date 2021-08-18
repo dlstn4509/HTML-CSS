5fc8d55fb418cbf3f0afdc7bff5b4de444a6c3b4
dlstn4509 <dlstn7609@hanmail.net>
2021-08-18 17:16:38 +0900

    firebase

---
+++ firebase/score/public/js/08.file.js
@@ -0,0 +1,53 @@
+/*************** global init ********************/
+var auth = firebase.auth();
+var database = firebase.database();
+var storage = firebase.storage();
+var googleAuth = new firebase.auth.GoogleAuthProvider();
+var dbRoot = database.ref('root');
+var stRoot = storage.ref().child('imgs');
+
+
+/*************** user function ******************/
+function genFile() {
+  return new Date().getTime()+"";
+}
+
+/*************** event callback *****************/
+function onAuthChanged(r) {
+  if(r) {
+    $('.bt-login').hide();
+    $('.bt-logout').show();
+  }
+  else {
+    $('.bt-login').show();
+    $('.bt-logout').hide();
+  }
+}
+
+function onLogin() {
+  auth.signInWithPopup(googleAuth);
+}
+
+function onLogout() {
+  auth.signOut();
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  var el = document.querySelector('input[name="upfile"]');
+  if (el.files.length) {
+    var file = el.files[0];
+    console.log(file);
+    var savename = genFile();
+    var uploader = stRoot.child(savename).put(file);
+  }
+}
+
+/*************** event init *********************/
+auth.onAuthStateChanged(onAuthChanged);
+$('form[name="uploadForm"]').submit(onSubmit);
+$('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogout);
+
+/*************** start init *********************/
+
