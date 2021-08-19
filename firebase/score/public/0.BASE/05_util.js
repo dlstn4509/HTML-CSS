227b7d9720c6bcbcb69ba59ae84a0453f859db92
dlstn4509 <dlstn7609@hanmail.net>
2021-08-19 09:48:45 +0900

    ..

--- firebase/score/public/0.BASE/05_util.js
+++ firebase/score/public/0.BASE/05_util.js
@@ -11,3 +11,25 @@ function zp(n) {
 	return (n < 10) ? '0'+n : n;
 	// 삼항 연산자 let a = 조건 ? 실행(참) : 실행(거짓)
 }
+
+function numberFormat(v) {           // 숫자 3자리마타 쉼표
+	return v.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getPlayTime(s) {
+	if(s >= 3600)
+	return zp(parseInt(s/3600)) + ':' + zp(parseInt((s%3600)/60)) + ':' + zp(s%60);
+	else
+	return zp(parseInt((s%3600)/60)) + ':' + zp(s%60);
+}
+
+/* function getPlayTime(s) {
+	return Math.floor(s/3600) + ':' + Math.floor(s%3600) + ':' + zp(s%60);
+} */
+
+/* function getPlayTime(s) {
+	if(Math.floor(s/60) >= 60)
+		return Math.floor(s/3600) + ':' + Math.floor(s%3600) + ':' + zp(s%60);
+	else
+		return zp(Math.floor(s/60)) + ':' + zp(s%60); 
+} */
