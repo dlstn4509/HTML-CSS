940a95dc707b3d82a814973f02f06b8aa7856431
dlstn4509 <dlstn7609@hanmail.net>
2021-08-19 11:21:36 +0900

    video upload

--- firebase/score/public/js/08.file.js
+++ firebase/score/public/js/08.file.js
@@ -3,18 +3,24 @@ var auth = firebase.auth();
 var database = firebase.database();
 var storage = firebase.storage();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
-var dbRoot = database.ref('root');
+var dbRoot = database.ref('root/uploads');
 var stRoot = storage.ref().child('imgs');
-
+var user = null;
+var allowExt = ['jpg', 'jpeg', 'png', 'gif', 'mp4'];
 
 /*************** user function ******************/
 function genFile() {
-  return new Date().getTime()+"";
+  var folder = moment().format('YYYYMMDDHH')
+  return {
+    folder: folder,
+    file: folder + '_' + uuidv4()
+  }
 }
 
 /*************** event callback *****************/
 function onAuthChanged(r) {
-  if(r) {
+  user = r;
+  if(user) {
     $('.bt-login').hide();
     $('.bt-logout').show();
   }
@@ -35,13 +41,64 @@ function onLogout() {
 function onSubmit(e) {
   e.preventDefault();
   var el = document.querySelector('input[name="upfile"]');
-  if (el.files.length) {
+  if (el.files.length && user) {
     var file = el.files[0];
+    if(allowExt.indexOf( file.name.split('.').pop().toLowerCase()) > -1 ) {
+      var savename = genFile();
+      var uploader = stRoot.child(savename.folder).child(savename.file).put(file);
+      uploader.on('state_changed', onUploading, onUploadError, onUploaded);
+    }
+    else alert('업로드 가능한 파일은 이미지 또는 mp4영상 입니다.')
+  }
+  else if(user === null) {
+    alert('로그인 후 시도해 주세요.')
+  }
+  else {
+    $('.input[name="upfile"]').focus();
+  }
+
+  function onUploading(snapshot) {
+    console.log('uploading', snapshot.bytesTransferred);
+    console.log('uploading', snapshot.totalBytes);
+    console.log('================================');
+    upfile = snapshot;
+  }
+  
+  function onUploaded() {
+    upfile.ref.getDownloadURL().then(onSuccess).catch(onError);
+  }
+  
+  function onUploadError(err) {
+    if(err.code === 'storage/unauthorized') location.href = '../403.html'
+    else console.log('error', err);
+  }
+  
+  function onSuccess(r) {
+    if(file.type.split('/')[0] === 'image') {
+      $('.main-img').attr('src', r).show();
+      $('.main-video').hide();
+    }
+    else if(file.type.split('/')[0] === 'video') {
+      $('.main-img').hide();
+      $('.main-video').attr('src', r).show();
+    }
+
+    var saveData = {
+      oriname: file.name,
+      savename: savename.file,
+      path: 'imgs/' + savename.folder,
+      type: file.type,
+      size: file.size
+    }
     console.log(file);
-    var savename = genFile();
-    var uploader = stRoot.child(savename).put(file);
+    dbRoot.push(saveData);
+  }
+  
+  function onError(err) {
+    console.log(err);
   }
 }
+
 
 /*************** event init *********************/
 auth.onAuthStateChanged(onAuthChanged);
