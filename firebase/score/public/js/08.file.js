a830537500c3f0fcbfe4adc0fcff058691a9d477
dlstn4509 <dlstn7609@hanmail.net>
2021-08-19 12:12:00 +0900

    08 file 게시판 완성

--- firebase/score/public/js/08.file.js
+++ firebase/score/public/js/08.file.js
@@ -20,13 +20,17 @@ function genFile() {
 /*************** event callback *****************/
 function onAuthChanged(r) {
   user = r;
-  if(user) {
+  if(user) {      // 로그인 했을 때
     $('.bt-login').hide();
     $('.bt-logout').show();
+    dbRoot.on('child_added', onAdded);
   }
-  else {
+  else {          // 로그아웃 했을 때
     $('.bt-login').show();
     $('.bt-logout').hide();
+    $('.list-wrap').empty();
+    $('.main-img').attr('src', '').hide()
+    $('.main-video').attr('src', '').hide()
   }
 }
 
@@ -74,6 +78,7 @@ function onSubmit(e) {
   }
   
   function onSuccess(r) {
+    $('.main-wrap').addClass('py-5');
     if(file.type.split('/')[0] === 'image') {
       $('.main-img').attr('src', r).show();
       $('.main-video').hide();
@@ -86,7 +91,7 @@ function onSubmit(e) {
     var saveData = {
       oriname: file.name,
       savename: savename.file,
-      path: 'imgs/' + savename.folder,
+      path: r,
       type: file.type,
       size: file.size
     }
@@ -99,6 +104,15 @@ function onSubmit(e) {
   }
 }
 
+function onAdded(r) {
+  var html = '<li class="list">';
+  if(r.val().type.indexOf('image') > -1 )
+    html += '<a href="'+r.val().path+'" target="_blank"><img src="'+r.val().path+'"></a>'
+  else
+    html += '<a href="'+r.val().path+'" target="_blank"><video src="'+r.val().path+'"></a>'
+  html += '</li>'
+  $(html).prependTo('.list-wrap');
+}
 
 /*************** event init *********************/
 auth.onAuthStateChanged(onAuthChanged);
