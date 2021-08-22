c3e685402fb5666f217adff11365686142eae331
dlstn4509 <dlstn7609@hanmail.net>
2021-08-22 10:35:49 +0900

    복습

---
+++ firebase/score/public/js/08.file copy.js
@@ -0,0 +1,17 @@
+/*************** global init ********************/
+var auth = firebase.auth();
+var db = firebase.database();
+var storage = firebase.storage();
+var auth = new firebase.auth.GoogleAuthProvider();
+
+/*************** user function ******************/
+
+
+/*************** event callback *****************/
+
+
+/*************** event init *********************/
+
+
+/*************** start init *********************/
+
