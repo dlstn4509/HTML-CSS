e8ed0ca2e88c3f75818e11cd8cf5dfaf6c58d8b4
dlstn4509 <dlstn7609@hanmail.net>
2021-08-26 17:31:02 +0900

    wearther 뿌리기

--- JS정리/js/14.weather.js
+++ JS정리/js/14.weather.js
@@ -1,38 +1,41 @@
-// 3c04f92240b9cdfbc2e0280668623b5d
-// https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=3c04f92240b9cdfbc2e0280668623b5d&units=metric
-// https://openweathermap.org/img/wn/01d@2x.png
-
-
-navigator.geolocation.getCurrentPosition(onCoordSuccess, onCoordError);
-
-function onCoordSuccess(r) {
-  let {latitude: lat, longitude: lon} = r.coords;
-  console.log(lat, lon);
-}
-
-function onCoordError(err) {
-console.log("🚀 ~ file: 14.weather.js ~ line 14 ~ onCoordError ~ err", err)
-
-}
-
-
-/* navigator.clipboard.writeText("<Hello World>").then(() => {
-  console.log('카피되었습니다.');
-}) */
-
-
-
 /*************** global init ********************/
-
+const appid = '3c04f92240b9cdfbc2e0280668623b5d'
+const url =  'https://api.openweathermap.org/data/2.5/weather'
+const icons =  ['https://openweathermap.org/img/wn/', '@2x.png']
+const param = {units: 'metric', appid}
 
 /*************** user function ******************/
+function init() {
+  navigator.geolocation.getCurrentPosition(onCoordSuccess, onCoordError);
+  function onCoordSuccess(r) {
+    let {latitude: lat, longitude: lon} = r.coords;
+    axios.get(url, {params: {...param, lat, lon}}).then(onGetWeather).catch(onError)
+  }
 
+  function onCoordError(err) {
+    axios.get(url, {params: {...param, id: '1832501'}}).then(onGetWeather).catch(onError)
+  } 
+}
 
 /*************** event callback *****************/
+function onGetWeather(r) {
+  let { main, name, weather } = r.data;
+  let { temp } = main;
+  let { main: title, description, icon } = weather[0];
+  const $wrap = $('.weather-wrap');
+  $wrap.find('.city span').text(name);
+  $wrap.find('.img-wp img').attr('src', icons[0] + icon + icons[1]);
+  $wrap.find('.temp-wp span').text(temp);
+  $wrap.find('.desc-wp .main').text(title);
+  $wrap.find('.desc-wp .description').text(description);
+}
 
+function onError(err) {
+  console.log(err);
+}
 
 /*************** event init *********************/
 
 
 /*************** start init *********************/
-
+init();
